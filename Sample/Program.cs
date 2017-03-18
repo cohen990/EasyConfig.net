0df9a7bb5b6540cd1605f43389789da3e5442493
Dan Cohen <dan.cohen@euromoneyplc.com>
2017-03-18 12:35:36 +0000

    improve sample

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -11,6 +11,19 @@ namespace EasyConfig.Sample
 
         static void Main(string[] args)
         {
+            /* Pass in command line arguments like the following:
+                uri_required=https://www.endpoint1.com uri_not_required=https://www.endpoint2.com string_required_commandline=myusername string_sensitive_required=willbeobfuscatedinlogs int_default=12345 bool_example=true custom_type=val1:thingy,val2:otherthingy
+            */
+            Config.RegisterTypeSupport(typeof(CustomType), (input) =>
+            {
+                var result = new CustomType();
+                var split = input.Split(',');
+                result.Val1 = split[0].Split(':')[1];
+                result.Val2 = split[1].Split(':')[1];
+
+                return result;
+            });
+
             _settings = Config.Populate<Settings>(args);
         }
 
@@ -34,6 +47,18 @@ namespace EasyConfig.Sample
 
             [AppConfig("AppConfigSetting")]
             public string InAppConfig;
+
+            [CommandLine("bool_example")]
+            public bool BoolExample { get; set; }
+
+            [CommandLine("custom_type")]
+            public CustomType CustomTypeExample { get; set; }
+        }
+
+        public class CustomType
+        {
+            public string Val1;
+            public string Val2;
         }
     }
 }
