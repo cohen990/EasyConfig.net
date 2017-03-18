eeee6d5e73262624b0de8edb16e8fda2ce7d9a6c
Samantha Cohen <samanthacohen294@gmail.com>
2017-03-18 11:34:42 +0000

    clean up support for properties

---
+++ EasyConfig/Reflection/MemberInfoReflector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace EasyConfig.Reflection
+{
+    public static class MemberInfoReflector
+    {
+        public static Type GetUnderlyingType(this MemberInfo member)
+        {
+            if(member == null)
+                throw new ArgumentNullException(nameof(member));
+
+            switch (member.MemberType)
+            {
+                case MemberTypes.Field:
+                    return ((FieldInfo)member).FieldType;
+                case MemberTypes.Property:
+                    return ((PropertyInfo)member).PropertyType;
+                default:
+                    throw new ArgumentException("Input MemberInfo must be of type FieldInfo or PropertyInfo");
+            }
+        }
+
+        public static IEnumerable<MemberInfo> GetFieldsAndProperties(MemberInfo[] members)
+        {
+            if (members == null || members.Length == 0)
+                return Enumerable.Empty<MemberInfo>();
+
+                var result = new List<MemberInfo>();
+
+            foreach(var member in members)
+            {
+                if (member.MemberType.HasFlag(MemberTypes.Field) || member.MemberType.HasFlag(MemberTypes.Property))
+                {
+                    result.Add(member);
+                }
+            }
+
+            return result;
+        }
+    }
+}
